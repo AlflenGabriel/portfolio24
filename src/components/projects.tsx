05f7d00646cbec5abdc48bfec79b639162819a1b
AlflenGabriel <alflen.gabriel@gmail.com>
2025-04-07 17:01:08 -0300

    update projects

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -34,10 +34,14 @@ const projects = () => {
 
               <div className="z-10 sm:order-2 sm:col-span-6">
                 <h3>
-                  <span className="inline-flex items-baseline font-medium leading-tight text-slate-200 cursor-not-allowed text-base">
+                  <a
+                    className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
+                    href="https://www.vetembrasilia.com.br"
+                    target="_blank"
+                  >
                     <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                    Veterinária em Brasília. (Em desenvolvimento)
-                  </span>
+                    Veterinária em Brasília
+                  </a>
                 </h3>
                 <p className="mt-2 text-sm leading-normal">
                   O foco deste projeto é criar um layout moderno e intuitivo,
@@ -70,7 +74,7 @@ const projects = () => {
                     target="_blank"
                   >
                     <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                    Alex Muniz Personal.
+                    Alex Muniz Personal
                   </a>
                 </h3>
                 <p className="mt-2 text-sm leading-normal">
@@ -105,7 +109,7 @@ const projects = () => {
                     target="_blank"
                   >
                     <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                    De Pata em Pata.
+                    De Pata em Pata
                   </a>
                 </h3>
                 <p className="mt-2 text-sm leading-normal">
@@ -139,7 +143,7 @@ const projects = () => {
                     target="_blank"
                   >
                     <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                    Fisio & Forma Web.
+                    Fisio & Forma Web
                   </a>
                 </h3>
                 <p className="mt-2 text-sm leading-normal">
@@ -160,7 +164,7 @@ const projects = () => {
                     href="/"
                   >
                     <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                    Web portfolio v2.
+                    Web portfolio v2
                   </a>
                 </h3>
                 <p className="mt-2 text-sm leading-normal">
@@ -190,7 +194,7 @@ const projects = () => {
                     href="https://alflengabriel.netlify.app/"
                   >
                     <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                    Web portfolio v1.
+                    Web portfolio v1
                   </a>
                 </h3>
                 <p className="mt-2 text-sm leading-normal">
